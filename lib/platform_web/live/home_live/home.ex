1d02146048728ac672ad2e8bc1f489a5b115205b
Aleksandar Diklic <rastasheep3@gmail.com>
2023-12-24 20:57:20 +0100

    Implement home page design

--- lib/platform_web/live/home_live/home.ex
+++ lib/platform_web/live/home_live/home.ex
@@ -8,46 +8,45 @@ defmodule PlatformWeb.HomeLive.Home do
   @impl true
   def mount(_params, _session, socket) do
     orgs = Accounts.list_organizations(preload: :workspaces)
-    {:ok, assign(socket, orgs: orgs)}
 
     {:ok,
      assign(socket,
-       orgs: orgs,
+       organizations: orgs,
        filter: "",
-       org_changes: Accounts.change_organization(%Organization{}),
-       ws_changes: Workspaces.change_workspace(%Workspace{})
+       organization_changes: Accounts.change_organization(%Organization{}),
+       workspace_changes: Workspaces.change_workspace(%Workspace{})
      )}
   end
 
   @impl true
-  def handle_event("create-org", %{"organization" => params}, socket) do
+  def handle_event("create-organization", %{"organization" => params}, socket) do
     case Accounts.create_organization(params, preload: :workspaces) do
       {:ok, org} ->
         socket
-        |> update(:orgs, fn orgs -> [org | orgs] end)
-        |> assign(org_changes: Accounts.change_organization(%Organization{}))
+        |> update(:organizations, fn orgs -> [org | orgs] end)
+        |> assign(organization_changes: Accounts.change_organization(%Organization{}))
         |> push_event("js-exec", %{to: "#org-modal", attr: "phx-remove"})
         |> noreply()
 
       {:error, %Ecto.Changeset{} = changeset} ->
         socket
-        |> assign(:org_changes, changeset)
+        |> assign(:organization_changes, changeset)
         |> noreply()
     end
   end
 
-  def handle_event("create-ws", %{"workspace" => params}, socket) do
+  def handle_event("create-workspace", %{"workspace" => params}, socket) do
     case Workspaces.create_workspace(params) do
       {:ok, _workspace} ->
         socket
-        |> update(:orgs, fn orgs -> Accounts.reload(orgs, preload: :workspaces) end)
-        |> assign(ws_changes: Workspaces.change_workspace(%Workspace{}))
+        |> update(:organizations, fn orgs -> Accounts.reload(orgs, preload: :workspaces) end)
+        |> assign(workspace_changes: Workspaces.change_workspace(%Workspace{}))
         |> push_event("js-exec", %{to: "#workspace-modal", attr: "phx-remove"})
         |> noreply()
 
       {:error, %Ecto.Changeset{} = changeset} ->
         socket
-        |> assign(:ws_changes, changeset)
+        |> assign(:workspace_changes, changeset)
         |> noreply()
     end
   end
